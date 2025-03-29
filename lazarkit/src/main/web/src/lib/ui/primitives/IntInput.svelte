40f67bc98f5a96d82d579e712ccf3434626c194f
monsieurlazar <dasidasi86@gmail.com>
2025-03-29 23:24:28 +0200

    more ui work

---
+++ lazarkit/src/main/web/src/lib/ui/primitives/IntInput.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  let {
+    currentValue = $bindable(),
+    onchange = (newValue: number) => {},
+  }: {
+    currentValue: number
+    onchange?: (newValue: number) => void
+  } = $props()
+
+  function setValue(newValue: number) {
+    currentValue = newValue
+    onchange(newValue)
+  }
+</script>
+
+<input type="number" bind:value={currentValue} />
+
+<style>
+</style>
