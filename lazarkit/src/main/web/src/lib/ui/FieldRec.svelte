415fe35837f7a5bf28b0c2fae90f23e9e1c908cd
monsieurlazar <dasidasi86@gmail.com>
2025-03-29 15:56:03 +0200

    ui testing

---
+++ lazarkit/src/main/web/src/lib/ui/FieldRec.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { JvmFieldInfo } from "$lib/socket.svelte"
+
+  let { line }: { line: JvmFieldInfo } = $props()
+</script>
+
+<p>
+  {line.className} | {line.fieldName} | {line.type} | {line.arrayType}
+</p>
+{#if ["INT", "DOUBLE", "STRING"].includes(line.type)}
+  <p>{line.currentValueString}</p>
+{:else if line.type == "ENUM"}
+  <p>{line.currentValueString} / {line.possibleValues}</p>
+{:else}
+  <p>TODO</p>
+  <p>{JSON.stringify(line, null, 2)}</p>
+{/if}
+
+<style>
+</style>
