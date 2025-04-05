901640e76ea5116cf56ae63c6f5268b324b5c754
lazar <dasidasi86@gmail.com>
2025-04-05 10:01:30 +0300

    flow registry

--- lazarkit/src/main/java/lol/lazar/lazarkit/flows/Flow.kt
+++ lazarkit/src/main/java/lol/lazar/lazarkit/flows/Flow.kt
@@ -2,6 +2,7 @@ package lol.lazar.lazarkit.flows
 
 import kotlinx.coroutines.sync.withLock
 import lol.lazar.lazarkit.core.GlobalEntities
+import java.util.UUID
 
 open class Flow(
     val action: suspend () -> Unit,
@@ -10,6 +11,11 @@ open class Flow(
 ) {
     constructor(action: suspend () -> Unit)
             : this(action = { action() }, description = "Flow")
+
+    val id = UUID.randomUUID()
+
+    var finishedTime: Long = -1L
+    val isFinished: Boolean get() = finishedTime != -1L
 
     open suspend fun execute() {
         val entity = entityId?.let { GlobalEntities.entities[it] }
@@ -20,6 +26,7 @@ open class Flow(
         } else {
             action()
         }
+        finishedTime = System.currentTimeMillis()
     }
 
     open fun describe(indent: Int): String =
