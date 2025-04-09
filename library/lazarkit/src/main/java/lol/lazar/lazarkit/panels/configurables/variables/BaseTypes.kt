4012fe02085f5f76049219f8aaa14d5e770ec648
monsieurlazar <dasidasi86@gmail.com>
2025-04-09 20:41:49 +0300

    refactoring

---
+++ library/lazarkit/src/main/java/lol/lazar/lazarkit/panels/configurables/variables/BaseTypes.kt
@@ -0,0 +1,20 @@
+package lol.lazar.lazarkit.panels.configurables.variables
+
+enum class BaseTypes {
+    INT,
+    LONG,
+    DOUBLE,
+    STRING,
+    BOOLEAN,
+    FLOAT,
+    ENUM,
+
+    UNKNOWN,
+
+    CUSTOM,
+    ARRAY,
+    LIST,
+    MAP,
+    GENERIC,
+    GENERIC_NO_ANNOTATION
+}
