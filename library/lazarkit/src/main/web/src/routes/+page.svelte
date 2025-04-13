8c0b26eef6e8beda8be539270e760e8f4e4e5314
lazar <dasidasi86@gmail.com>
2025-04-14 00:04:33 +0300

    homepage

--- library/lazarkit/src/main/web/src/routes/+page.svelte
+++ library/lazarkit/src/main/web/src/routes/+page.svelte
@@ -1,15 +1,40 @@
 <script lang="ts">
-  import Section from "$ui/primitives/Section.svelte"
-  import TextTypedInput from "$ui/primitives/TextTypedInput.svelte"
+  import { gamepads } from "$lib"
+  import GamepadDrawing from "$lib/ui/GamepadDrawing.svelte"
+  import GameField from "$ui/widgets/fields/GameField.svelte"
+  import Flows from "$ui/widgets/Flows.svelte"
+  import Graph from "$ui/widgets/Graph.svelte"
+  import PlaybackHistory from "$ui/widgets/PlaybackHistory.svelte"
+  import { OpModeControl, Telemetry, Configurables } from "$widgets"
 </script>
 
-<h1>Welcome to Custom Dashboard</h1>
+<section>
+  <div>
+    <OpModeControl />
+    <PlaybackHistory />
+    {#if gamepads.current != null}
+      <GamepadDrawing gamepad={gamepads.gamepads[0]} />
+    {/if}
+  </div>
+  <div>
+    <GameField />
+    <Telemetry />
+  </div>
+  <div>
+    <Configurables />
+  </div>
+  <div>
+    <Graph />
+  </div>
+</section>
 
-<Section title="Homepage">
-  <p>Check the time: <a href="/time">Click here</a></p>
-  <p>Check the dash: <a href="/control">Click here</a></p>
-  <p>Hello: <a href="/hello">Click here</a></p>
-  <p>Game Field: <a href="/field">Click here</a></p>
-
-  <TextTypedInput text="da" type="TEST" />
-</Section>
+<style>
+  section {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    overflow-x: auto;
+    scroll-behavior: smooth;
+    height: 100%;
+    padding: 0.5rem;
+  }
+</style>
