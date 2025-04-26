16edc9d535bbe455a1f563a86be116ff85f99e2b
monsieurlazar <dasidasi86@gmail.com>
2025-04-26 14:06:20 +0300

    Update index.ts

--- library/lazarkit/src/main/web/src/lib/ui/widgets/index.ts
+++ library/lazarkit/src/main/web/src/lib/ui/widgets/index.ts
@@ -1,5 +1,4 @@
 import OpModeControl from "./OpModeControl.svelte"
 import Telemetry from "./Telemetry.svelte"
 import Configurables from "./configurables/Configurables.svelte"
-import Flows from "./Flows.svelte"
-export { OpModeControl, Telemetry, Configurables, Flows }
+export { OpModeControl, Telemetry, Configurables }
