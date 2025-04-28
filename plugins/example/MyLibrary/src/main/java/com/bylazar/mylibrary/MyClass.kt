5a339129db1b93b65cde55435e583c74fa3e58fa
lazar <dasidasi86@gmail.com>
2025-04-28 15:03:31 +0300

    preparing registry

--- plugins/example/MyLibrary/src/main/java/com/bylazar/mylibrary/MyClass.kt
+++ plugins/example/MyLibrary/src/main/java/com/bylazar/mylibrary/MyClass.kt
@@ -1,4 +1,12 @@
 package com.bylazar.mylibrary
 
-class MyClass {
+import com.bylazar.ftcontrol.panels.plugins.PanelsPlugin
+
+class MyClass : PanelsPlugin {
+    override val name: String = "Lazar's Example Plugin"
+
+    override fun onRegister() {
+        println("DASH: ran internal plugin register")
+    }
+
 }
