05c33da4e65f546b1ad0c15b97b37867c113d216
monsieurlazar <dasidasi86@gmail.com>
2025-04-30 19:46:56 +0300

    fixed plugins finding

--- library/lazarkit/src/main/java/com/bylazar/ftcontrol/panels/plugins/PluginManager.kt
+++ library/lazarkit/src/main/java/com/bylazar/ftcontrol/panels/plugins/PluginManager.kt
@@ -2,86 +2,65 @@ package com.bylazar.ftcontrol.panels.plugins
 
 import android.content.Context
 import com.bylazar.ftcontrol.panels.CorePanels
-import com.bylazar.ftcontrol.panels.configurables.utils.extractClassNamesFromDex
-import dalvik.system.DexClassLoader
-import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.nio.ByteOrder
-import java.nio.channels.FileChannel
 
 object PluginManager {
-    var plugins = mutableMapOf<String, PanelsPlugin>()
+    val plugins = mutableMapOf<String, PanelsPlugin>()
+    private val finder = ClassFinder()
 
     fun onRegister(corePanels: CorePanels) {
-        val context = ModContext()
-        plugins.values.forEach { it.onRegister(context) }
+        val modContext = ModContext()
+        plugins.forEach { (id, plugin) ->
+            try {
+                plugin.onRegister(modContext)
+            } catch (e: Exception) {
+                println("DASH: Failed to register plugin $id")
+                e.printStackTrace()
+            }
+        }
     }
 
     fun loadPlugins(context: Context) {
-        plugins = mutableMapOf<String, PanelsPlugin>()
-        println("DASH: Length: ${context.assets.list("plugins")?.size}")
-        context.assets.list("plugins")?.forEach { pluginName ->
-            println("DASH: Found plugin in assets: $pluginName")
+        println("DASH: Loading plugins...")
+        plugins.clear()
+        finder.apkPath = context.packageCodePath
 
-            val isDexFile = pluginName.endsWith(".dex")
+        val foundClasses = finder.getAllClasses
 
-            if (!isDexFile) {
-                println("DASH: Skipping non-dex file: $pluginName")
-                return@forEach
-            }
+        println("DASH: Found ${foundClasses.size} classes:")
 
+        for (classEntry in foundClasses) {
             try {
-                val tempFile = File.createTempFile("plugin_", ".dex", context.cacheDir)
-                context.assets.open("plugins/$pluginName").use { input ->
-                    FileOutputStream(tempFile).use { output ->
-                        input.copyTo(output)
+                val clazz = Class.forName(classEntry.className)
+
+                if (PanelsPlugin::class.java.isAssignableFrom(clazz)) {
+                    println("DASH: Found plugin implementation: ${clazz.name}")
+
+                    val constructor = clazz.getDeclaredConstructor()
+                    val pluginInstance = constructor.newInstance() as PanelsPlugin
+
+                    val originalId = pluginInstance.id
+                    var uniqueId = originalId
+                    var suffix = 1
+
+                    while (plugins.containsKey(uniqueId)) {
+                        println("DASH: Plugin ID '$originalId' is already registered. Renaming...")
+                        uniqueId = "$originalId${suffix++}"
                     }
+
+                    pluginInstance.id = uniqueId
+                    plugins[uniqueId] = pluginInstance
+
+                    println("DASH: Successfully registered plugin: ${clazz.name} with ID '$uniqueId'")
                 }
-
-                val classLoader = DexClassLoader(
-                    tempFile.absolutePath,
-                    context.cacheDir.absolutePath,
-                    null,
-                    context.classLoader
-                )
-
-                val byteBuffer = FileInputStream(tempFile).channel.use { channel ->
-                    channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size()).apply {
-                        order(ByteOrder.LITTLE_ENDIAN)
-                    }
-                }
-
-                val classNames = byteBuffer.extractClassNamesFromDex()
-                classNames.forEach{ println("DASH: $it") }
-
-                classNames.forEach { className ->
-                    try {
-                        val pluginClass = classLoader.loadClass(className)
-
-                        if (PanelsPlugin::class.java.isAssignableFrom(pluginClass)) {
-                            println("DASH: Found plugin implementation: $className")
-
-                            val pluginInstance = pluginClass.getDeclaredConstructor().newInstance() as PanelsPlugin
-
-                            if (plugins.containsKey(pluginInstance.id)) {
-                                println("DASH: Plugin with ID ${pluginInstance.id} is already registered")
-                                pluginInstance.id += "DUPLICATED"
-                            }
-
-                            plugins[pluginInstance.id] = pluginInstance
-
-                            println("DASH: Successfully registered plugin: $className")
-                        }
-                    } catch (e: Exception) {
-
-                    }
-                }
-
-                tempFile.delete()
-            } catch (e: Exception) {
+            } catch (e: ClassNotFoundException) {
+                println("DASH: Class not found: ${classEntry.className}")
                 e.printStackTrace()
-                println("DASH: Error loading plugin $pluginName: ${e.message}")
+            } catch (e: ClassCastException) {
+                println("DASH: Class does not implement PanelsPlugin: ${classEntry.className}")
+                e.printStackTrace()
+            } catch (e: Exception) {
+                println("DASH: Unexpected error loading plugin class: ${classEntry.className}")
+                e.printStackTrace()
             }
         }
     }
