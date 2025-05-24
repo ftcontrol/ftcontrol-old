7cb794ad60f5b349a6c3d0169aef6daaf6b07d28
monsieurlazar <dasidasi86@gmail.com>
2025-05-24 15:04:38 +0300

    made resizing predict on empty cells

--- library/lazarkit/src/main/web/src/lib/ui/grid/ResizingOverlay.svelte
+++ library/lazarkit/src/main/web/src/lib/ui/grid/ResizingOverlay.svelte
@@ -3,21 +3,48 @@
   import type { PresetManager } from "./logic/preset.svelte"
 
   let { gridManager }: { gridManager: PresetManager } = $props()
+
+  function getClosest(x: number, y: number) {
+    if (modular.showWidget(x, y)) return { x, y }
+
+    let minDist = Infinity
+    let closest = { x, y }
+
+    for (let iy = 1; iy <= gridManager.cellsY; iy++) {
+      for (let ix = 1; ix <= gridManager.cellsX; ix++) {
+        if (modular.showWidget(ix, iy)) {
+          const dist = Math.abs(ix - x) + Math.abs(iy - y)
+          if (dist < minDist) {
+            minDist = dist
+            closest = { x: ix, y: iy }
+          }
+        }
+      }
+    }
+
+    return closest
+  }
 </script>
 
 {#each Array.from( { length: gridManager.cellsX * gridManager.cellsY } ) as _, index}
   {@const y = Math.floor(index / gridManager.cellsX) + 1}
   {@const x = (index % gridManager.cellsX) + 1}
   {@const id = gridManager.widgetsMap[y][x]}
-  {#if id == null || modular.resizing.resizingModule?.id == id || modular.moving.movingModule?.id == id}
+  {#if id == null || modular.resizing.resizingModule?.id == id || modular.moving.movingModule?.id == id || false}
+    {@const isShown = modular.showWidget(x, y)}
+    {@const closest = getClosest(x, y)}
     <div
       class="overlay-item"
-      class:isShown={modular.showWidget(x, y)}
-      data-x={x}
-      data-y={y}
+      class:other={!isShown}
+      class:isShown={modular.moving.isMov || modular.resizing.isResizing}
+      data-x={closest.x}
+      data-y={closest.y}
       style="grid-row: {y} / span 1; grid-column: {x} / span 1;"
     >
-      <div class="color"></div>
+      <div class="color">
+        {closest.x}
+        {closest.y}
+      </div>
     </div>
   {/if}
 {/each}
@@ -30,7 +57,7 @@
   }
   .overlay-item.isShown {
     display: block;
-    opacity: 0.5;
+    opacity: 0;
   }
   .color {
     width: 100%;
